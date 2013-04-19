fe58a16feafe5b89bd528cbe9a2cfc3f4468a990
Johnathan Smith <johnathan.smith@uftwf.org>
2013-04-19 10:05:49 -0400

    fixed spelling errors

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/Documents/dotfiles                    # dotfiles directory
 olddir=~/Documents/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim bash_profile gitconfig gitingore"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim bash_profile gitconfig gitignore"    # list of files/folders to symlink in homedir
 
 ##########
 
